fa7f8caf2bb2fb2d06b49bb4ceb28d43bd7ffc4b
Clavijomo <jonathanclavijo0907@gmail.com>
2024-11-21 11:35:21 -0500

    :sparkles: Validation ready and use enviroment variables

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
 async function bootstrap() {
-    console.log('JWT_SECRET_KEY', process.env.JWT_SECRET_KEY);
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(
         new ValidationPipe({
